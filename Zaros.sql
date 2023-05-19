fb44d9657b51673e2775b676d2b42d6c321c8d03
Jose Augusto <ja834136@gmail.com>
2023-05-19 15:27:27 -0300

    alter table

--- Zaros.sql
+++ Zaros.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [IF NOT EXISTS] usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
         user_id int not null auto_increment,
         user_cpf varchar(14) not null,
         user_nome varchar(80) not null,
@@ -9,25 +9,27 @@ CREATE TABLE [IF NOT EXISTS] usuarios (
         primary key (user_id)
 );
 
-CREATE TABLE [IF NOT EXISTS] lojas (
+CREATE TABLE IF NOT EXISTS lojas (
         user_id int not null,
         lj_nome_loja varchar(80) not null,
         lj_desc varchar(80) not null,
         lj_cnpj varchar(14) not null,
-        primary key (user_id)
+        primary key (user_id),
+        ALTER TABLE lojas ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES usuarios (user_id)
 );
 
-CREATE TABLE [IF NOT EXISTS] pedidos (
+CREATE TABLE IF NOT EXISTS pedidos (
         pdd_id int not null auto_increment,
         user_id int not null,
         pdd_nota_fiscal varchar(50) not null,
         pdd_qtd int not null,
         pdd_preco decimal(10,2) not null,
         pdd_tamanho varchar(20) not null,
-        primary key (pdd_id)
+        primary key (pdd_id),
+        ALTER TABLE pedidos ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES usuarios (user_id)
 );
 
-CREATE TABLE [IF NOT EXISTS] produtos (
+CREATE TABLE IF NOT EXISTS produtos (
         pdt_id int not null auto_increment,
         pdt_preco decimal (10,2) not null,
         pdt_imagem varchar(80) not null,
@@ -35,24 +37,24 @@ CREATE TABLE [IF NOT EXISTS] produtos (
         pdt_nome varchar(80) not null,
         user_id int not null,
         primary key (pdt_id),
-        FOREIGN KEY (user_id) REFERENCES lojas (user_id)
+        ALTER TABLE produtos ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES lojas (user_id)
 );
 
-CREATE TABLE [IF NOT EXISTS] produtos_pedidos (
+CREATE TABLE IF NOT EXISTS produtos_pedidos (
         pdd_id int not null,
         pdt_id int not null,
         pp_qtd_total int not null,
         pp_preco_total decimal(10,2) not null,
         primary key (pdd_id, pdt_id),
-        FOREIGN KEY (pdd_id) REFERENCES pedidos (pdd_id),
-        FOREIGN KEY (pdt_id) REFERENCES produtos (pdt_id)
+        ALTER TABLE produtos_pedidos ADD CONSTRAINT FOREIGN KEY (pdd_id) REFERENCES pedidos (pdd_id),
+        ALTER TABLE produtos_pedidos ADD CONSTRAINT FOREIGN KEY (pdt_id) REFERENCES produtos (pdt_id)
 )
 
-CREATE TABLE [IF NOT EXISTS] enderecos (
+CREATE TABLE IF NOT EXISTS enderecos (
         user_id int not null,
         end_logradouro varchar(40) not null,
         end_num varchar(5) not null,
         end_tipo varchar(50) not null,
-        end_bairro varchar(30) not null
-        FOREIGN KEY (user_id) REFERENCES usuarios (user_id)
+        end_bairro varchar(30) not null,
+         ALTER TABLE enderecos ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES usuarios (user_id)
 );
